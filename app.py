0fe395100fcc187dd1b4db462a10b413a49eec4b
Jesus Salas <salas.mena.jesus@gmail.com>
2024-02-08 14:32:02 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, abort
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    hostname = socket.gethostname()
+    html = f"<h1>Hello World!!</h1> This page is served from <b>{hostname}</b><hr>v1.0.2"
+    return html
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", port=5000)
